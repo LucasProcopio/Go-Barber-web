b3644e2c3bcd0bfac46c3f722bb6b694cbecf908
Lucas Henrique Procopio <procmode.dev@gmail.com>
2019-10-04 16:46:15 -0300

    :sparkles: feat: Add navigation

---
+++ src/routes/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
+
+import SignUp from '../pages/SignUp';
+import SignIn from '../pages/SignIn';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={SignIn} />
+      <Route path="/register" component={SignUp} />
+
+      <Route path="/dashboard" component={Dashboard} />
+      <Route path="/profile" component={Profile} />
+    </Switch>
+  );
+}
