dff8a85c7e02e996af4d34db2d3df5b63aa5b5d9
Lucas Henrique Procopio <procmode.dev@gmail.com>
2019-10-06 21:43:24 -0300

    :sparkles: feat: Add Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '~/assets/logoPurple.svg';
+import { Container, Content, Profile } from './styles';
+
+export default function Header() {
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <img src={logo} alt="Go barber" />
+          <Link to="/dashboard">DASHBOARD</Link>
+        </nav>
+
+        <aside>
+          <Profile>
+            <div>
+              <strong>Lucas PROC</strong>
+              <Link to="/profile">My Profile</Link>
+            </div>
+            <img
+              src="https://api.adorable.io/avatars/50/abott@adorable.png"
+              alt="Avatar"
+            />
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
