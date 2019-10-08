cc18e39b22173d8dd26780a795bf8c31a94c9562
Lucas Henrique Procopio <procmode.dev@gmail.com>
2019-10-08 14:24:02 -0300

    :sparkles: feat: Add user dashboard
    List of appointments on user dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,39 +1,88 @@
-import React from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
+
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+import {
+  format,
+  subDays,
+  addDays,
+  setHours,
+  setMinutes,
+  setSeconds,
+  isBefore,
+  isEqual,
+  parseISO,
+} from 'date-fns';
+import { utcToZonedTime } from 'date-fns-tz';
 import api from '~/services/api';
 
 import { Container, Time } from './styles';
 
+const range = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
 export default function Dashboard() {
+  const [schedule, setschedule] = useState([]);
+  const [date, setDate] = useState(new Date());
+
+  const dateFormatted = useMemo(() => format(date, 'MMMM d'), [date]);
+
+  useEffect(() => {
+    async function loadSchedule() {
+      const response = await api.get('schedule', {
+        params: { date },
+      });
+      console.tron.log(response);
+      const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+      const data = range.map(hour => {
+        const formattedDate = setSeconds(
+          setMinutes(setHours(date, hour), 0),
+          0
+        );
+        const compareDate = utcToZonedTime(formattedDate, timezone);
+
+        return {
+          time: `${hour}:00h`,
+          past: isBefore(compareDate, new Date()),
+          appointment: response.data.appointment.find(a =>
+            isEqual(parseISO(a.date), compareDate)
+          ),
+        };
+      });
+      setschedule(data);
+    }
+
+    loadSchedule();
+  }, [date]);
+
+  function handleAddDays() {
+    setDate(addDays(date, 1));
+  }
+
+  function handleRemoveDays() {
+    setDate(subDays(date, 1));
+  }
+
   return (
     <Container>
       <header>
-        <button type="button">
+        <button type="button" onClick={handleRemoveDays}>
           <MdChevronLeft size={36} color="#FFF" />
         </button>
-        <strong> may 31</strong>
-        <button type="button">
+        <strong>{dateFormatted}</strong>
+        <button type="button" onClick={handleAddDays}>
           <MdChevronRight size={36} color="#FFF" />
         </button>
       </header>
 
       <ul>
-        <Time past>
-          <strong>08:00</strong>
-          <span>Lucas Proc</span>
-        </Time>
-        <Time available>
-          <strong>08:00</strong>
-          <span>Em aberto</span>
-        </Time>
-        <Time>
-          <strong>10:00</strong>
-          <span>Lucas Proc</span>
-        </Time>
-        <Time>
-          <strong>11:00</strong>
-          <span>Lucas Proc</span>
-        </Time>
+        {schedule.map(time => (
+          <Time key={time.time} past={time.past} available={!time.appointment}>
+            <strong>{time.time}</strong>
+            <span>
+              {time.appointment ? time.appointment.user.name : 'Open'}
+            </span>
+          </Time>
+        ))}
       </ul>
     </Container>
   );
